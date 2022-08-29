07bf398d8c9f970a0d7e3241c6aa5eb600bbbd87
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-29 22:49:51 +1100

    Create run.py

---
+++ run.py
@@ -0,0 +1,16 @@
+import os,sys,json
+
+def clear():
+    os.system('clear')
+    time.sleep(1)
+    
+def back():
+    os.system('main')
+    time.sleeo(1)
+
+def banner():
+    print('stop ini bukan script hahahahahahahahahahaahahahahaahh')
+def main():
+    banner()
+    
+    main()
